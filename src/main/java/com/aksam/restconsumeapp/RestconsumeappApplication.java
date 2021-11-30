426c00ee108c5c6fe8245b9344da762a72744368
Akanimo Sam Okure <akan.okure@gmail.com>
2021-11-30 10:50:40 +0000

    first commit

---
+++ src/main/java/com/aksam/restconsumeapp/RestconsumeappApplication.java
@@ -0,0 +1,13 @@
+package com.aksam.restconsumeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestconsumeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestconsumeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aksam/restconsumeapp/RestconsumeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksam.restconsumeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestconsumeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
